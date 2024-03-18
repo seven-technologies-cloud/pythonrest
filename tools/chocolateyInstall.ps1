fbc68aa329fc6685546e1bcdce17b20281977167
daflongustavo <gustavodaflon@seventechnologies.cloud>
2024-03-18 16:38:04 -0300

    updating chocolatey project

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,19 +1,24 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
+$packageName = 'pythonrest'
+$url = 'https://github.com/daflongustavo/pythonrest/releases/download/0.1.0/pythonrest.exe'
+$checksum = 'zDPNsTLfRPZ7s1ZoD0qU3wB/WWmWXw1A5GY4LQjq6O8='
+$checksumType = 'sha256'
+$toolsDir = "$(Get-ToolsLocation)\$packageName"
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/daflongustavo/pythonrest/releases/download/0.1.0/pythonrest.exe'
-$checksum64 = 'zDPNsTLfRPZ7s1ZoD0qU3wB/WWmWXw1A5GY4LQjq6O8='
+Write-Host "Starting $packageName installation..."
 
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  unzipLocation  = $toolsDir
-  fileType       = 'exe'
-  url64bit       = $url64
-  softwareName   = 'pythonREST Command Line Interface*'
-  checksum64     = $checksum64
-  checksumType64 = 'sha256'
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).ExeInstall.log`""
-  validExitCodes = @(0, 3010, 1641)
+if (-not (Test-Path $toolsDir)) {
+    Write-Host "Directory not found in $toolsDir, creating a new one..."
+    New-Item -ItemType Directory -Path $toolsDir -Force
+    Write-Host "Directory created."
+} else {
+    Write-Host "Installation directory already exists, moving to next steps..."
 }
 
-Install-ChocolateyPackage @packageArgs
+
+Write-Host "Starting download from $url..."
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath "$toolsDir\pythonrest.exe" -Url $url -Checksum $checksum -ChecksumType $checksumType
+Write-Host "Download successful"
+
+
+Write-Host "Successful installation! Try $packageName using the command 'pythonrest version' in your terminal."
