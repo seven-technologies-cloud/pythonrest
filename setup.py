fa90ab6cab5caec86755b67454e54831755c0dba
Suspir0n <evandrojunior1615@gmail.com>
2024-08-30 12:32:21 -0300

    add install_requires

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ import os
 
 
 def find_packages_custom(start_directory='.', include=None, prefix='pythonrest'):
-  if include is None:
-      include = [start_directory]
+    if include is None:
+        include = [start_directory]
 
-  packages = [prefix]
-  for dir in include:
-      for root, dirs, files in os.walk(dir):
-          if '__init__.py' in files:
-              package_path = f"{prefix}.{os.path.relpath(root, start_directory).replace(os.sep, '.')}"
-              packages.append(package_path)
-  return packages
+    packages = [prefix]
+    for dir in include:
+        for root, dirs, files in os.walk(dir):
+            if '__init__.py' in files:
+                package_path = f"{prefix}.{os.path.relpath(root, start_directory).replace(os.sep, '.')}"
+                packages.append(package_path)
+    return packages
+
 
 def list_files_by_extension(directory='.', extension=()):
     package_data = {}
@@ -27,10 +28,6 @@ def list_files_by_extension(directory='.', extension=()):
                 package_data[new_path] = values
     return package_data
 
-# Include requirements.txt in package_data
-package_data = list_files_by_extension(extension=('.yaml', '.txt', '.html', '.md'))
-package_data['pythonrest'] = ['requirements.txt']
-
 setup(
     name='pythonrest3',
     version='0.1.9',
@@ -40,10 +37,25 @@ setup(
     maintainer='Seven Technologies Cloud',
     keywords=['api', 'rest api', 'database', 'python',
               'mysql', 'mssql', 'postgres', 'aurora', 'mariadb'],
-    package_data=list_files_by_extension(extension=('.yaml', '.txt', '.html', '.md')),
-    packages=find_packages_custom(include=['apigenerator', 'databaseconnector', 'domaingenerator']),
+    package_data=list_files_by_extension(
+        extension=('.yaml', '.txt', '.html', '.md')),
+    packages=find_packages_custom(
+        include=['apigenerator', 'databaseconnector', 'domaingenerator']),
     package_dir={'pythonrest': '.'},
-    install_requires=[],
+    install_requires=[
+        'typer==0.9.0',
+        'PyYAML==6.0.1',
+        'parse==1.20.0',
+        'mergedeep==1.3.4',
+        'pymysql==1.1.0',
+        'rsa==4.9',
+        'cryptography==42.0.7',
+        'cffi==1.16.0',
+        'pycparser==2.22',
+        'pyasn1==0.6.0',
+        'psycopg2-binary==2.9.9',
+        'pymssql==2.2.11',
+    ],
     entry_points={
         'console_scripts': [
             'pythonrest=pythonrest:app',
