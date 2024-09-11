a478b72e21d639b0ecd14138dd872f4d1ce96c52
leo-viana <106110473+leo-viana@users.noreply.github.com>
2024-09-11 20:40:51 -0300

    QOL for redoc

--- apigenerator/resources/1 - Project/1 - BaseProject/Project/app.py
+++ apigenerator/resources/1 - Project/1 - BaseProject/Project/app.py
@@ -3,12 +3,12 @@ from src.e_Infra.g_Environment.EnvironmentVariables import *
 
 # Controller Imports #
 from src.a_Presentation.d_Swagger.SwaggerController import *
-from src.a_Presentation.c_Redoc.RedocController import *
+from src.a_Presentation.f_Redoc.RedocController import *
+from src.a_Presentation.b_Custom.FlaskAdminPanelController import *
 from src.a_Presentation.b_Custom.OptionsController import *
 from src.a_Presentation.b_Custom.SQLController import *
 from src.a_Presentation.b_Custom.BeforeRequestController import *
 from src.a_Presentation.b_Custom.ExceptionHandlerController import *
-from src.a_Presentation.b_Custom.FlaskAdminPanelController import *
 
 
 # LocalHost run #
--- apigenerator/resources/1 - Project/1 - BaseProject/Project/src/a_Presentation/f_Redoc/RedocController.py
+++ apigenerator/resources/1 - Project/1 - BaseProject/Project/src/a_Presentation/f_Redoc/RedocController.py
@@ -4,7 +4,8 @@ from src.e_Infra.b_Builders.FlaskBuilder import *
 # Methods to create main Redoc docs #
 @redoc_blueprint.route('/redoc')
 def redoc():
-    return render_template('redoc.html')
+    redoc_spec_url = '/redoc/spec'
+    return render_template('redoc.html', spec_url=redoc_spec_url)
 
 
 @redoc_blueprint.route('/redoc/spec')
@@ -13,4 +14,3 @@ def spec():
     with open(yaml_path, 'r') as yaml_file:
         yaml_content = yaml_file.read()
     return yaml_content
-
