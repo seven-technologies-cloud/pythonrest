4adff32dd6fb718998beeb6475dddfcdc3aaa093
Suspir0n <52609767+Suspir0n@users.noreply.github.com>
2024-12-18 15:42:51 -0300

    chore: remove validation

--- apigenerator/g_Utils/ReplaceColumnName.py
+++ apigenerator/g_Utils/ReplaceColumnName.py
@@ -2,8 +2,6 @@ def adding_replace_in_column_name_with_spaces(column_name: str) -> str:
     separators = ["-", "_", " ", ".", "/", "\\", ":", "~", "*", "+", "|", "@"]
 
     for separator in separators:
-        if '\\' in column_name:
-            column_name = column_name.replace('\\', '\\\\')
         if separator in column_name:
             column_name = column_name.replace(separator, '_')
             return column_name
