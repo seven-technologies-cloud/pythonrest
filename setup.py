2efe4d8df73836bb76624d4ec613a85a34ed73f0
leo-viana <106110473+leo-viana@users.noreply.github.com>
2024-12-20 18:15:35 -0300

    Incrementing version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def list_files_by_extension(directory='.', extension=()):
 
 setup(
     name='pythonrest3',
-    version='0.2.1',
+    version='0.2.6',
     description='A CLI tool that generates a complete API using a connection string for supported databases: mysql, mssql, mariadb and postgres',
     author='Seven Technologies Cloud',
     author_email='admin@seventechnologies.cloud',
